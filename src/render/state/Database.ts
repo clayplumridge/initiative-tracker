01bd93f43f7c2fcb29b46caba3dd4a89bd726c1b
John Casey <gilles.degrasse@gmail.com>
2021-03-14 16:14:57 -0400

    comment out db stuff

--- src/render/state/Database.ts
+++ src/render/state/Database.ts
@@ -8,20 +8,22 @@ const db: LowdbSync<any> = low(adapter);
 
 export class Database {
     constructor() {
-        //Check if we have read/write permissions to the database
-        //Then, check if the database is initalized, and if it isn't, initalize it
-        fs.accessSync(dataFile, fs.constants.R_OK | fs.constants.W_OK);
-        fs.open(dataFile, "r", (err, fd) => {
-            if (err) throw err;
-            const readData: Uint8Array = new Uint8Array();
-            fs.read(fd, readData, 0, 1, 0, () => {
-                fs.close(fd, err => {
-                    if (err) throw err;
-                });
-                if (!readData[0]) {
-                    db.defaults({ actorTemplates: [], encounters: [] }).write();
-                }
-            });
-        });
+        db.defaults({ actorTemplates: [], encounters: [] }).write();
+
+        // //Check if we have read/write permissions to the database
+        // //Then, check if the database is initalized, and if it isn't, initalize it
+        // fs.accessSync(dataFile, fs.constants.R_OK | fs.constants.W_OK);
+        // fs.open(dataFile, "r", (err, fd) => {
+        //     if (err) throw err;
+        //     const readData: Uint8Array = new Uint8Array();
+        //     fs.read(fd, readData, 0, 1, 0, () => {
+        //         fs.close(fd, err => {
+        //             if (err) throw err;
+        //         });
+        //         if (!readData[0]) {
+        //             db.defaults({ actorTemplates: [], encounters: [] }).write();
+        //         }
+        //     });
+        // });
     }
 }
